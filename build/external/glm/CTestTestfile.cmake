81a9d762b5466c35c2d4dfd7445f6f5bc8a066a0
Snorre Hansen <snorrehaha@gmail.com>
2025-01-11 14:47:31 +0100

    Init commit

--- build/external/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/snorre/myProjects/grapichs/template/external/glm
-# Build directory: /home/snorre/myProjects/grapichs/template/build/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/framework/GLFWApplication/CMakeFiles/GLFWApplication.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GLFWApplication.
-# This may be replaced when dependencies are built.
